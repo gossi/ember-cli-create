6a2e2b486d4212b3b8ef09ee0ec242090fb9212d
hakubo <bukaj.kelo+github@gmail.com>
2018-10-18 20:26:55 +0200

    (Broken) Check for Ember-cli version

--- src/presets/octane.js
+++ src/presets/octane.js
@@ -1,7 +1,18 @@
+let VersionChecker = require('ember-cli-version-checker');
+let checker = new VersionChecker(this);
+let emberCliVersion = checker.for('ember-cli');
+let experiments;
+
+if (emberCliVersion.gte('3.5.0')) {
+	experiments = ['MODULE_UNIFICATION'];
+} else {
+	experiments = ['MODULE_UNIFICATION', 'BROCCOLI_2', 'SYSTEM_TEMP']
+};
+
 module.exports = {
 	type: 'app',
 	addons: ['ember-decorators', 'sparkles-component', 'sparkles-decorators', 'ember-auto-import'],
-	experiments: ['MODULE_UNIFICATION'],
+	experiments: experiments,
 	features: ['template-only-glimmer-components'],
 	welcome: false
 };
