cd2049704cfdd584db0efdc2833bd2bea811ae47
Jakub Olek <bukaj.kelo+github@gmail.com>
2018-10-18 10:39:04 +0200

    These are enabled by default in Ember CLI 3.5

--- src/presets/octane.js
+++ src/presets/octane.js
@@ -1,7 +1,7 @@
 module.exports = {
 	type: 'app',
 	addons: ['ember-decorators', 'sparkles-component', 'sparkles-decorators', 'ember-auto-import'],
-	experiments: ['MODULE_UNIFICATION', 'BROCCOLI_2', 'SYSTEM_TEMP'],
+	experiments: ['MODULE_UNIFICATION'],
 	features: ['template-only-glimmer-components'],
 	welcome: false
 };
